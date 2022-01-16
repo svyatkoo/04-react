dc532f61e165068d7ff55f5b0330c6ba060781ec
SviatoslavHeletii <svyatkoo@gmail.com>
2022-01-16 12:53:35 +0200

    cw-react-02

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,16 +6,20 @@ const Form = ({getFilter}) => {
     const formInfo = (e) => {
         const eData = {...form, [e.target.name]: e.target.value}
         setForm({...setForm, ...eData})
-        getFilter(eData)
+        // getFilter(eData)
     }
 
+    const onSubmit = (e) => {
+        e.preventDefault();
+        getFilter(form)
+    }
     return (
         <div>
             <form>
                 <label>Name:<input type="text" name={"name"} value={form.name} onChange={formInfo}/></label>
                 <label>Username:<input type="text" name={"username"} value={form.username} onChange={formInfo}/></label>
                 <label>Email:<input type="text" name={"email"} value={form.email} onChange={formInfo}/></label>
-                <button>Find</button>
+                <button onClick={onSubmit}>Find</button>
             </form>
         </div>
     );
